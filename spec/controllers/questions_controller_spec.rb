31050bd0d96e57ac70e09cba7289a101647546fb
aldav99 <aldav99@yandex.ru>
2018-09-17 06:50:52 +0300

    Исправленный spec show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
-    before do
-      get :index
-    end
+    before { get :index }
 
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
@@ -20,13 +18,13 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #show' do
     let(:question) { create(:question) }
 
+    before { get :show, params: { id: question } }
+
     it 'assigns the requested question to @question' do
-      get :show, id: question
       expect(assigns(:question)).to eq question
     end
 
     it 'renders show view' do
-      get :show, id: question
       expect(response).to render_template :show
     end
   end

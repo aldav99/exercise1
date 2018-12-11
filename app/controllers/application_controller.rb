197bd860b2bf33e2bd8ae3fc3d265c1d8d1fe547
aldav99 <aldav99@yandex.ru>
2018-12-11 06:12:50 +0300

    gon_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :gon_user, unless: :devise_controller?
+
+  def gon_user
+    gon.user_signed_in = user_signed_in?
+    gon.current_user_id = current_user.id if current_user
+  end
 end

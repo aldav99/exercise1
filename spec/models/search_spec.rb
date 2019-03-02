6c1fc59096a5591a9af24c37ab391d5260cdd218
aldav99 <aldav99@yandex.ru>
2019-03-02 14:53:37 +0300

    Sphinx try1

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -2,13 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Search, type: :model do
 
-  it 'send request' do
+  it 'send request ALL' do
     request = "sidekiq"
     escape_query = ThinkingSphinx::Query.escape(request)
 
-    expect(ThinkingSphinx::Query).to receive(:escape).with(request).and_call_original
-    expect(ThinkingSphinx).to receive(:search).with(escape_query)
+    expect(ThinkingSphinx).to receive(:search).with(escape_query).and_call_original
 
     Search.query(request, "all")
+  end
+
+  %w( question answer comment user).each do |choice|
+    it "send request with right area:  #{choice}" do
+      request = "sidekiq"
+      escape_query = ThinkingSphinx::Query.escape(request)
+    
+      expect(choice.classify.constantize).to receive(:search).with(escape_query).and_call_original
+
+      Search.query(request, choice)
+    end
+  end
+
+  it "send request with invalid area" do
+    request = "sidekiq"
+    escape_query = ThinkingSphinx::Query.escape(request)
+  
+    expect(ThinkingSphinx).to_not receive(:search).with(escape_query)
+
+    Search.query(request, "another")
   end
 end

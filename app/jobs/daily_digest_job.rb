bf6efa342b4ba641afcd63b1539c2f779b109934
aldav99 <aldav99@yandex.ru>
2019-07-05 14:12:23 +0300

    07072019 deliver_later(wait: 2.minutes)

--- app/jobs/daily_digest_job.rb
+++ app/jobs/daily_digest_job.rb
@@ -16,7 +16,7 @@ class DailyDigestJob < ActiveJob::Base
 
     if questions.present?
       User.find_each do |user|
-        DailyMailer.digest(user, questions).deliver_later
+        DailyMailer.digest(user, questions).deliver_later(wait: 2.minutes)
       end
     end
   end

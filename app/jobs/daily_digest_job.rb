a34c3e63a92e38d242a480bb54c1d07b5046c78e
aldav99 <aldav99@yandex.ru>
2019-07-17 07:32:40 +0300

    17.07 created_at: Time.now - 24.hours

--- app/jobs/daily_digest_job.rb
+++ app/jobs/daily_digest_job.rb
@@ -12,7 +12,10 @@ class DailyDigestJob < ActiveJob::Base
   # queue_as :default
 
   def perform
-    questions = Question.where(created_at: Time.now.all_day).pluck(:title)
+    # questions = Question.where(created_at: Time.now.all_day).pluck(:title)
+
+    questions = Question.where(created_at: Time.now - 24.hours).pluck(:title)
+
 
     if questions.present?
       User.find_each do |user|
